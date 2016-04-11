1b9ce5076579c0a1eca4e1a4d25d5260f8ac3433
Debarshi Dasgupta <debarshi.dg@gmail.com>
2016-04-11 11:10:31 +0530

    Initial version.

---
+++ RSSFeed/src/com/rss/tests/ReadTest.java
@@ -0,0 +1,19 @@
+package com.rss.tests;
+
+import com.rss.model.Feed;
+import com.rss.model.FeedMessage;
+import com.rss.read.RSSFeedParser;
+
+public class ReadTest {
+  public static void main(String[] args) {
+    RSSFeedParser parser = new RSSFeedParser("https://www-356.ibm.com/partnerworld/wps/servlet/pwdyn/Feed?cmsId=isv_ast_nws_rss_feeds&attachmentName=pwcloud.xml");
+    Feed feed = parser.readFeed();
+    System.out.println(feed);
+    for (FeedMessage message : feed.getMessages()) {
+      System.out.println(message);
+
+    }
+
+  }
+} 
+
