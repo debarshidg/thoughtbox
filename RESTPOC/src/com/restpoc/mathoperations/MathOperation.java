3b642b1e832fd592829d2821ff2a57f050ff17e8
debarshi.dg <debarshi.dg@gmail.com>
2016-12-18 00:19:59 +0530

    post service

--- RESTPOC/src/com/restpoc/mathoperations/MathOperation.java
+++ RESTPOC/src/com/restpoc/mathoperations/MathOperation.java
@@ -1,6 +1,7 @@
 package com.restpoc.mathoperations;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,25 +12,40 @@ import org.json.JSONObject;
 
 @Path("/mathOperation")
 public class MathOperation {
-	
+
 	@GET
 	@Produces("application/json")
 	public Response mathOperation() throws JSONException {
 		System.out.println("enter addNumbers");
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("SUM", 10);
-		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n"
+				+ jsonObject;
 		return Response.status(200).entity(result).build();
 	}
 
-	  @Path("{f}/{y}/{z}")
-	  @GET
-	  @Produces("application/json")
-	  public Response mathOperation(@PathParam("f") float f, @PathParam("y") float y, @PathParam("y") float z) throws JSONException {
+	@Path("{f}/{y}/{z}")
+	@GET
+	@Produces("application/json")
+	public Response mathOperation(@PathParam("f") float f,
+			@PathParam("y") float y, @PathParam("y") float z)
+			throws JSONException {
 		System.out.println("enter addNumbers" + f + "" + y + "" + z);
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("SUM", f + y + z);
-		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n"
+				+ jsonObject;
 		return Response.status(200).entity(result).build();
 	}
+	@POST
+	@Path("/post")
+	@Produces("application/json")
+	public Response mathOperationPost(String msg)throws JSONException {
+		System.out.println("Param " + msg);
+		JSONObject jsonObject = new JSONObject();
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n"
+				+ jsonObject;
+		return Response.status(200).entity(result).build();
+	}	
+	
 }
